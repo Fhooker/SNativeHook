35da613fcd263e3d00e6c01d12103e34c846c85d
sanbo <sanbo.xyz@gmail.com>
2020-02-18 18:26:19 +0800

    update build files

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo build start...
 
+rem build files
+rmdir /s/q libs obj
+
 set NDK_PROJECT_PATH=.
 
 ndk-build
+pause
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,9 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := Hooker
-LOCAL_SRC_FILES := 	Hooker.cpp	\
-					Debug.cpp	\
-					PosixMemory.cpp
+LOCAL_SRC_FILES :=     Hooker.cpp \
+                    Debug.cpp \
+                    PosixMemory.cpp
 LOCAL_INCLUDES  := $(LOCAL_PATH)/jni
 LOCAL_CPPFLAGS+= -Wno-error=format-security
-include $(BUILD_STATIC_LIBRARY)
+
+#下面一句是编译成动态库，即so库
+include $(BUILD_SHARED_LIBRARY)
+
+# include $(BUILD_STATIC_LIBRARY)
+LOCAL_PATH := $(call my-dir)
